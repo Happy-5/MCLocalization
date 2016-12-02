943a981a5c34ffd07836456d173d0605251186b8
Taufik Obet <mailtaufik@gmail.com>
2016-12-02 15:00:59 +0700

    Add nullability annotations

--- Classes/MCLocalization.h
+++ Classes/MCLocalization.h
@@ -13,31 +13,31 @@
 
 @interface MCLocalization : NSObject
 
-@property (nonatomic, retain) id<MCLocalizationDataSource>dataSource;
+@property (nonatomic, retain) _Nullable id<MCLocalizationDataSource>dataSource;
 
-@property (nonatomic, copy) NSString * language;
-@property (nonatomic, readonly) NSArray * supportedLanguages;
-@property (nonatomic, readonly) NSString * systemLanguage;
+@property (nonatomic, copy) NSString * _Nonnull language;
+@property (nonatomic, readonly) NSArray * _Nonnull supportedLanguages;
+@property (nonatomic, readonly) NSString * _Nonnull systemLanguage;
 
 // String using this placeholder will be returned in case a key in not found
 // {key} and {language} substring will be substituted for corresponding values
 // Default value is nil
-@property (nonatomic, copy) NSString * noKeyPlaceholder;
+@property (nonatomic, copy) NSString * _Nullable noKeyPlaceholder;
 
-+ (NSString *)stringForKey:(NSString *)key;
-+ (NSString *)stringForKey:(NSString *)key withPlaceholders:(NSDictionary *)placeholders;
-+ (MCLocalization *)sharedInstance;
++ (NSString * _Nonnull)stringForKey:(NSString * _Nonnull)key;
++ (NSString * _Nonnull)stringForKey:(NSString * _Nonnull)key withPlaceholders:(NSDictionary * _Nonnull)placeholders;
++ (MCLocalization *_Nonnull)sharedInstance;
 
-+ (void)loadFromURL:(NSURL *)JSONFileURL defaultLanguage:(NSString *)defaultLanguage;
-+ (void)loadFromLanguageURLPairs:(NSDictionary *)languageURLPairs defaultLanguage:(NSString *)defaultLanguage;
++ (void)loadFromURL:(NSURL *_Nonnull)JSONFileURL defaultLanguage:(NSString *_Nonnull)defaultLanguage;
++ (void)loadFromLanguageURLPairs:(NSDictionary *_Nonnull)languageURLPairs defaultLanguage:(NSString *_Nonnull)defaultLanguage;
 
 - (void)reloadStrings;
 
 // Methods exposed for extending purposes
-- (NSDictionary *)stringsForLanguage:(NSString *)language;
-- (NSString *)stringForKey:(NSString *)key language:(NSString *)language;
+- (NSDictionary *_Nonnull)stringsForLanguage:(NSString *_Nonnull)language;
+- (NSString *_Nonnull)stringForKey:(NSString *_Nonnull)key language:(NSString *_Nonnull)language;
 
 // Legacy methods
-+ (void)loadFromJSONFile:(NSString *)fileName defaultLanguage:(NSString *)defaultLanguage;
++ (void)loadFromJSONFile:(NSString *_Nonnull)fileName defaultLanguage:(NSString *_Nonnull)defaultLanguage;
 
 @end
